dd87f291c4acd1916b41dfd8b4643db938d20ad6
akmal2409 <akmalalikhujaev@gmail.com>
2021-03-20 14:02:33 +0100

    Moved eureka and rabbit mq configurations to git based repo

--- product-service/src/main/java/tech/talci/productservice/controller/TestController.java
+++ product-service/src/main/java/tech/talci/productservice/controller/TestController.java
@@ -1,12 +1,14 @@
 package tech.talci.productservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
+@RefreshScope
 public class TestController {
 
     @Value("${test.name}")
