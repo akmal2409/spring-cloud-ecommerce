a9b27af3bdf85771d93659e0f079ec5e5e84b7c5
akmal2409 <akmalalikhujaev@gmail.com>
2021-03-21 18:36:47 +0100

    Resolved dependency conflicts in inventory service, downgraded to spring-cloud 2020.01

--- order-service/src/main/java/tech/talci/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/tech/talci/orderservice/OrderServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RefreshScope
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
