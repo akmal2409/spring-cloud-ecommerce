dfa1d56e11d38f00024e2eb9b52d0081fe34c810
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2021-03-27 11:36:34 +0100

    Added open feign and a client for communications between Order and Inventory services, added Token Interceptor

--- order-service/src/main/java/tech/talci/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/tech/talci/orderservice/OrderServiceApplication.java
@@ -1,10 +1,15 @@
 package tech.talci.orderservice;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,4 +20,13 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    @Bean
+    public RequestInterceptor requestTokenInterceptor() {
+        return requestTemplate -> {
+            JwtAuthenticationToken token = (JwtAuthenticationToken) SecurityContextHolder.getContext()
+                    .getAuthentication();
+
+            requestTemplate.header("Authorization", "Bearer " + token.getToken().getTokenValue());
+        };
+    }
 }
--- order-service/src/main/java/tech/talci/orderservice/dto/OrderDto.java
+++ order-service/src/main/java/tech/talci/orderservice/dto/OrderDto.java
@@ -11,5 +11,5 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
-    private List<OrderLineItems> orderLineItems;
+    private List<OrderLineItems> orderLineItemsList;
 }
