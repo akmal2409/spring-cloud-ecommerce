edddfd7974060956d6a0e8e3294f12c4cd9de3bc
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2021-04-03 18:44:41 +0200

    Added a circuit breaker to Order Service

--- order-service/src/main/java/tech/talci/orderservice/service/OrderService.java
+++ order-service/src/main/java/tech/talci/orderservice/service/OrderService.java
@@ -2,6 +2,8 @@ package tech.talci.orderservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import tech.talci.orderservice.client.InventoryClient;
 import tech.talci.orderservice.dto.OrderDto;
@@ -9,6 +11,7 @@ import tech.talci.orderservice.model.Order;
 import tech.talci.orderservice.repository.OrderRepository;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @Service
 @Slf4j
@@ -16,14 +19,24 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+
 
     public String placeOrder(OrderDto orderDto) {
         if (orderDto == null) {
             throw new RuntimeException("Order is not present");
         }
-        boolean allProductsInStock = orderDto.getOrderLineItemsList()
+
+        Resilience4JCircuitBreaker circuitBreaker = this.circuitBreakerFactory
+                .create("inventory");
+
+        Supplier<Boolean> stockSupplier = () -> orderDto.getOrderLineItemsList()
                 .stream()
                 .allMatch(item -> this.inventoryClient.isInStock(item.getSkuCode()));
+
+        circuitBreaker.run(stockSupplier, throwable -> handleErrorCase());
+
+        boolean allProductsInStock = stockSupplier.get();
 
         if (allProductsInStock) {
             Order order = Order.builder()
@@ -40,5 +53,9 @@ public class OrderService {
         } else {
             return "Order failed. One of the products is not in stock. Please try again";
         }
+    }
+
+    private Boolean handleErrorCase() {
+        return Boolean.FALSE;
     }
 }
