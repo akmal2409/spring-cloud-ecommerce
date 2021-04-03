6729197a39fd3e9c70ac1e3d0ae8903ee49051c0
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2021-04-03 20:36:57 +0200

    Initialized notification service

---
+++ notification-service/src/test/java/tech/talci/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.talci.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
